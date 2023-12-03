d99119b6090aa1f34079f98ef133fcd9af3f4a8e
ZNS <zns05082003@gmail.com>
2023-12-03 22:39:54 +0300

    Реализован оснвоной функционал программы.

---
+++ interface.h
@@ -0,0 +1,17 @@
+#pragma once
+#include "Train.h"
+
+class Interface
+{
+    Train *data;
+    int size;
+
+public:
+    Interface();
+    ~Interface();
+    void add();
+    void del();
+    void sort();
+    void search();
+    void print();
+};
